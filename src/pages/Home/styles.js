ea7aee1dc91af6a6b4ceb5ba7bf0f453a8e245ff
Felipe <felipejunger7@gmail.com>
2023-07-27 16:49:57 -0300

    aplicando brand na Home

--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -16,12 +16,41 @@ export const Container = styled.div`
     background-color: ${({ theme }) => theme.COLORS.BACKGROUND_800};
 `;
 
-export const Brand = styled.div``;
+export const Brand = styled.div`
+    grid-area: brand;
+    
+    display: flex;
+    justify-content: center;
+    align-items: center;
 
-export const Menu = styled.ul``;
+    border-bottom-width: 1px;
+    border-bottom-style: solid;
+    border-bottom-color: ${({ theme }) => theme.COLORS.BACKGROUND_700};
 
-export const earch = styled.div``;
+    background-color: ${({ theme }) => theme.COLORS.BACKGROUND_900};
 
-export const Content = styled.div``;
+    > h1 {
+        font-size: 24px;
+        color: ${({ theme }) => theme.COLORS.ORANGE};
+    }
+`;
 
-export const NewNote = styled.button``;
+export const Menu = styled.ul`
+    grid-area: menu;
+    
+`;
+
+export const Search = styled.div`
+    grid-area: search;
+   
+`;
+
+export const Content = styled.div`
+    grid-area: content;
+    
+`;
+
+export const NewNote = styled.button`
+    grid-area: newnote;
+    
+`;
