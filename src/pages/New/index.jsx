80b97c3dc34d2b46c512a05d6b34061f65c73edc
Felipe <felipejunger7@gmail.com>
2023-07-29 17:11:49 -0300

    finalizando o New

--- src/pages/New/index.jsx
+++ src/pages/New/index.jsx
@@ -3,6 +3,7 @@ import { Input } from '../../components/Input';
 import { Textarea } from '../../components/Textarea';
 import { NoteItem } from '../../components/NoteItem';
 import { Section } from '../../components/Section';
+import { Button } from '../../components/Button';
 
 import { Container, Form } from './styles';
 
@@ -25,8 +26,18 @@ export function New() {
                         <NoteItem value="https://rocketseat.com.br"/>
                         <NoteItem isNew placeholder="Novo link"/>
                     </Section>
+
+                    <Section title="Marcadores">
+                       <div className='tags'> 
+                        <NoteItem value="react"/>
+                        <NoteItem isNew placeholder="Nova tag"/>
+                        </div>
+                    </Section>
+
+                    <Button title="Salvar" />
                 </Form>
             </main>
         </Container>
     );
 }
+
