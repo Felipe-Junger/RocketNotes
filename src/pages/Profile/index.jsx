7990bee4128d547bb18babc599711e2d9fd62b6b
Felipe <felipejunger7@gmail.com>
2023-07-29 15:28:14 -0300

    finalizando tela Profile

--- src/pages/Profile/index.jsx
+++ src/pages/Profile/index.jsx
@@ -1,10 +1,10 @@
-import { FiArrowLeft, FiUser, FiMail, FiLock } from 'react-icons/fi';
+import { FiArrowLeft, FiUser, FiMail, FiLock, FiCamera } from 'react-icons/fi';
 
 import { Input } from '../../components/Input';
 import { Button } from '../../components/Button';
 
 
-import { Container, Form } from './styles';
+import { Container, Form, Avatar } from './styles';
 
 export function Profile() {
     return(
@@ -16,6 +16,21 @@ export function Profile() {
             </header>
 
             <Form>
+                <Avatar>
+                    <img 
+                        src="https://github.com/Felipe-Junger.png" 
+                        alt="Foto do usuário" 
+                    />
+
+                    <label  htmlFor="avatar">
+                        <FiCamera />
+
+                        <input 
+                            id="avatar"
+                            type="file"
+                        />
+                    </label>
+                </Avatar>
                 <Input 
                     placeholder="Nome"
                     type="text"
@@ -45,3 +60,4 @@ export function Profile() {
         </Container>
     );
 }
+
