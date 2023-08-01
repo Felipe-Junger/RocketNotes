127c8431fab6d5dda551bc5f1177cf260ba2c474
Felipe <felipejunger7@gmail.com>
2023-08-01 17:46:02 -0300

    criando funções de autenticação para tela SignIn

--- src/pages/SignIn/index.jsx
+++ src/pages/SignIn/index.jsx
@@ -1,4 +1,4 @@
-
+import { useState } from 'react';
 import { FiMail, FiLock } from 'react-icons/fi';
 import { Link } from 'react-router-dom';
 import { Input } from '../../components/Input';
@@ -9,8 +9,14 @@ import { useAuth } from '../../hooks/auth';
 import { Container, Form, Background } from './styles';
 
 export function SignIn() {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
 
-    const data = useAuth();
+    const { signIn } = useAuth();
+
+    function handleSignIn() {
+        signIn({ email, password });
+    }
     
     return(
         <Container>
@@ -24,15 +30,17 @@ export function SignIn() {
                     placeholder = "E-mail"
                     type = "text"
                     icon = {FiMail}
+                    onChange = {e => setEmail(e.target.value)}
                 />
 
                 <Input 
                     placeholder = "Senha"
                     type = "password"
                     icon = {FiLock}
+                    onChange = {e => setPassword(e.target.value)}
                 />
                 
-                <Button title = "Entrar" />
+                <Button title = "Entrar" onClick={handleSignIn} />
 
                 <Link to="/register">
                     Criar conta
